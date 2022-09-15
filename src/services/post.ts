11b3142db978883baabf6dbb97686ed8fc36b488
sixtus iwuchukwu <sixtusiwuchukwu21@gmail.com>
2022-09-15 02:22:21 +0100

    remove crfprvention

--- src/services/post.ts
+++ src/services/post.ts
@@ -1,7 +1,7 @@
 import {Request,Response,NextFunction} from "express"
 import PostDataSource from "./posts/datasource"
 const AwsUpload =async(req:Request,res:Response,next:NextFunction)=>{
-    console.log(req.body)
+    console.log(req.body,req)
 //    let response = await  new PostDataSource().createPost(req.body,(req as any).user)
 //    res.send(response)
 }
