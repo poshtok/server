521f198ad63de6b69fd34a3111bd36141cc8e976
sixtus iwuchukwu <sixtusiwuchukwu21@gmail.com>
2022-09-16 10:41:45 +0100

    updated user id

--- src/server.ts
+++ src/server.ts
@@ -23,8 +23,8 @@ const esClient = new Client({ node: 'http://localhost:9200' })
   const app:Application = express();
   app.use(compression());
   app.use(cors());
-  app.use(express.json({limit:"100mb"}))
-  app.use(express.urlencoded({limit:"100mb", extended:true}))
+  app.use(express.json({limit:"500mb"}))
+  app.use(express.urlencoded({limit:"500mb", extended:true}))
   app.use(isAuth)
   app.post("/uploadpost",AwsUpload)
   app.post("/contactus",contactUs);
